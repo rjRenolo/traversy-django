5f587bdb2deb7396e604c3a4fcbb9bb9aa51934f
rjRenolo <rjrenolo14@gmail.com>
2020-06-04 11:23:15 +0800

    add admin page for questions

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question, Choice
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
+
+# choices within question pages
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldSets = [(None, {'fields': ['question_text']}), 
+    ('Date Information', {'fields': ['pub_date'], 'classes':['collapes']}),]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
